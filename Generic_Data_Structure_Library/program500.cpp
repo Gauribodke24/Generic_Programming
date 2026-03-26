fcf62e7e817e75767c8e109c499ee196031c8f76
Gauribodke24 <gauribodke24@gmail.com>
2026-03-26 18:38:13 +0530

    Add Generic DSLibrary

---
+++ Generic_Data_Structure_Library/program500.cpp
@@ -0,0 +1,914 @@
+///////////////////////////////////////////////////////
+//         Generalised Data Structure Library
+///////////////////////////////////////////////////////
+
+/*
+---------------------------------------------------------------------------------------------------------------
+Type                    Name of class for node                      Name of class for Functionality
+---------------------------------------------------------------------------------------------------------------
+Singly Linear            SinglyLLLnode                               SinglyLLL      DONE        
+Singly Circular          SinglyCLLnode                               SinglyCLL              
+Doubly Linear            DoublyLLLnode                               DoublyLLL      DONE
+Doubly Circular          DoublyCLLnode                               DoublyCLL
+Stack                    Stacknode                                   Stack          DONE        
+Queue                    Queuenode                                   Queue          DONE      
+----------------------------------------------------------------------------------------------------------------
+*/
+
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////
+//
+//      Singly Linear LinkedList using Generic Approach
+//
+///////////////////////////////////////////////////////////
+
+
+#pragma pack(1)
+
+template<class T>
+class SinglyLLLnode
+{
+    public:
+        T data;
+        SinglyLLLnode<T> *next;
+
+        SinglyLLLnode(T no)
+        {
+            this->data = no;
+            this->next = NULL;
+        }
+};
+
+template<class T>
+class SinglyLLL
+{
+    private:            
+        SinglyLLLnode<T> * first;
+        int iCount;
+
+    public:    
+        SinglyLLL();
+        
+        void InsertFirst(T no);
+        void InsertLast(T no); 
+        void DeleteFirst();
+        void DeleteLast();
+        void Display();
+        int Count();
+        void InsertAtPos(T, int);
+        void DeleteAtPos(int);
+        
+};
+
+template <class T>
+SinglyLLL<T> :: SinglyLLL()
+{
+    cout<<"Object of SinglyLL gets Created.\n";
+    this -> first = NULL;
+    this -> iCount = 0;
+}
+
+template <class T>
+void SinglyLLL<T> :: InsertFirst(T no)
+{
+    SinglyLLLnode <T> * newn = NULL;
+    newn = new SinglyLLLnode <T> (no);
+    
+    newn -> next = this -> first;    
+    this -> first = newn;
+
+    this -> iCount++;
+}
+
+template<class T>
+void SinglyLLL<T> :: InsertLast(T no)
+{
+    SinglyLLLnode <T> * newn = NULL;
+    SinglyLLLnode <T> * temp = NULL;
+
+    newn = new SinglyLLLnode <T> (no);
+
+    if(this -> iCount == 0)        
+    {
+        this -> first = newn;
+    } 
+    else
+    {
+        temp = this -> first;
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+
+    this -> iCount++;
+}
+
+template <class T>
+void SinglyLLL<T> :: DeleteFirst()
+{
+    SinglyLLLnode<T> * temp = NULL;
+
+    if(this -> first == NULL)        
+    {
+        return;
+    }
+
+    else if(this -> first -> next == NULL)    
+    {
+        delete this -> first;
+        this -> first = NULL;
+    }
+
+    else
+    {
+        temp = this -> first;
+        this -> first = this -> first -> next;
+        delete(temp);
+    }
+
+    this -> iCount--;
+}
+
+template <class T>
+void SinglyLLL<T> :: DeleteLast()
+{
+    SinglyLLLnode<T> * temp = NULL;
+
+    if(this -> first == NULL)
+    {
+        return;
+    }
+
+    else if(this -> first -> next == NULL)
+    {
+        delete this -> first;
+        this -> first = NULL;
+    }
+
+    else
+    {
+        temp = this -> first;
+
+        while(temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        delete temp -> next;
+        temp -> next = NULL;
+    }
+    this -> iCount--;
+}
+
+
+template <class T>
+void SinglyLLL<T> :: Display()
+{
+    SinglyLLLnode<T> * temp = NULL;
+    int iCnt = 0;
+
+    temp = this -> first;
+
+    for(iCnt = 1; iCnt <= this -> iCount; iCnt++)      
+    {
+        cout<<" | " << temp->data << " | ->";
+        temp = temp -> next;
+    }
+
+    cout<<"NULL"<<"\n";
+}
+
+template <class T>
+int SinglyLLL<T> :: Count()
+{
+    return this -> iCount;
+}
+
+template <class T>
+void SinglyLLL<T> :: InsertAtPos(T no, int pos)
+{
+    SinglyLLLnode<T> * newn = NULL;
+    SinglyLLLnode<T> * temp = NULL;
+
+    int iCnt = 0;
+
+    if(pos < 1 || pos > this -> iCount+1)
+    {
+        cout<<"Invalid Position \n";
+        return;
+    }
+
+    if(pos == 1)
+    {
+        this -> InsertFirst(no);
+    }
+    else if(pos == this -> iCount+1)
+    {
+        this -> InsertLast(no);
+    }
+    else
+    {
+        newn = new SinglyLLLnode<T>(no);
+
+        temp = this -> first;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+        
+        newn -> next = temp -> next;
+        temp -> next = newn;
+        
+        this -> iCount++;
+    }
+}
+
+template <class T>
+void SinglyLLL<T> :: DeleteAtPos(int pos)
+{
+    SinglyLLLnode<T> * temp = NULL;
+    SinglyLLLnode<T> * target = NULL;
+
+    int iCnt = 0;
+
+    if(pos < 1 || pos > this -> iCount)
+    {
+        cout<<"Invalid Position \n";
+        return;
+    }
+
+    if(pos == 1)
+    {
+        this -> DeleteFirst();
+    }
+
+    else if(pos == this -> iCount)
+    {
+        this -> DeleteLast();
+    }
+
+    else
+    {
+        temp = this->first;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp->next;
+        }
+        
+        target = temp->next;
+
+        temp->next = target->next;
+        delete target;
+    
+        
+        this->iCount--;
+    }
+}
+
+///////////////////////////////////////////////////////////
+//
+//     Doubly Linear LinkedList using Generic Approach
+//
+///////////////////////////////////////////////////////////
+
+#pragma pack(1)
+
+template <class T>
+class DoublyLLLnode
+{
+    public:
+        T data;
+        DoublyLLLnode *next;
+        DoublyLLLnode *prev;
+
+        DoublyLLLnode(T no)
+        {
+            this -> data = no;
+            this -> next = NULL;
+            this -> prev = NULL;
+        }
+};
+
+template <class T>
+class DoublyLLL
+{
+    private:
+        DoublyLLLnode<T> * first;  
+        int iCount;
+
+    public:
+        DoublyLLL();
+
+        void InsertFirst(T);
+        void InsertLast(T);
+        void InsertAtPos(T,int);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int);
+
+        void Display();
+        int Count();
+};
+
+template<class T>
+DoublyLLL<T> :: DoublyLLL()
+{
+    cout<<"LinkedList Gets Created \n";
+    this -> first = NULL;
+    this -> iCount = 0;
+}
+
+template <class T>
+void DoublyLLL<T> :: InsertFirst(T no)
+{
+    DoublyLLLnode<T> *newn = NULL;
+
+    newn = new DoublyLLLnode<T>(no);
+
+    if(this -> first == NULL)
+    {
+        this -> first = newn;
+    }
+
+    else
+    {
+        newn -> next = this -> first;
+        this -> first -> prev = newn;
+        this -> first = newn;
+    }
+
+    this -> iCount++;
+}
+
+template <class T>
+void DoublyLLL<T> :: InsertLast(T no)
+{
+    DoublyLLLnode<T> *newn = NULL;
+    DoublyLLLnode<T> *temp = NULL;
+
+    newn = new DoublyLLLnode<T>(no);
+
+    if(this -> first == NULL)
+    {
+        this -> first = newn;
+    }
+
+    else
+    {
+        temp = this -> first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+        newn -> prev = temp;
+    }
+
+
+    this -> iCount++;
+}
+
+template<class T>
+void DoublyLLL<T> :: InsertAtPos(T no,int pos)
+{
+    DoublyLLLnode<T> *temp = NULL;
+    int iCnt  = 0;
+    DoublyLLLnode<T> *newn = NULL;
+
+    if((pos < 1) || (pos > this->iCount+1))
+    {
+        cout<<"Invalid Position \n";
+        return; 
+    }
+
+    if(pos == 1)
+    {
+        this -> InsertFirst(no);
+    }
+
+    else if(pos == this -> iCount+1)
+    {
+        this -> InsertLast(no);
+    }
+
+    else
+    {
+        newn = new DoublyLLLnode<T>(no);
+
+        temp = this -> first;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+
+        newn -> next = temp -> next;
+        temp -> next -> prev = newn;
+
+        temp -> next = newn;
+        newn -> prev = temp;
+
+        this -> iCount++;
+    }
+}
+
+template <class T>
+void DoublyLLL<T> :: DeleteFirst()
+{
+
+    if(this -> first ==  NULL)                 
+    {
+        return;
+    }
+
+    else if(this -> first -> next == NULL)     
+    {
+        delete this -> first;
+        this -> first = NULL;
+    }
+
+    else                                    
+    {
+        this -> first = this -> first -> next;
+        delete this -> first -> prev;
+        this -> first -> prev = NULL;
+    } 
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyLLL<T> :: DeleteLast()
+{
+    DoublyLLLnode<T> *temp = NULL;
+
+    if(this -> first ==  NULL)                 
+    {
+        return;
+    }
+
+    else if(this -> first -> next == NULL)      
+    {
+        delete this -> first;
+        this -> first = NULL;
+    }
+
+    else                                    
+    {
+        temp = this -> first;
+
+        while(temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        delete temp -> next;
+        temp -> next = NULL;
+    }  
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyLLL<T> :: DeleteAtPos(int pos)
+{
+    DoublyLLLnode<T> *temp = NULL;
+    int iCnt  = 0;
+
+    if((pos < 1) || (pos > this -> iCount))
+    {
+        cout<<"Invalid Position \n";
+        return; 
+    }
+
+    if(pos == 1)
+    {
+        this -> DeleteFirst();
+    }
+
+    else if(pos == this -> iCount)
+    {
+        this -> DeleteLast();
+    }
+
+    else
+    {
+        temp = this -> first;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = temp -> next -> next;
+        delete temp -> next -> prev;
+        temp -> next -> prev = temp;
+
+        this -> iCount--;
+    }    
+}
+
+template <class T>
+void DoublyLLL<T> :: Display()
+{
+    DoublyLLLnode<T> *temp = NULL;
+
+    temp = this -> first;
+
+    cout<<"\n NULL <=>";
+    while(temp != NULL)
+    {
+        cout<<" | "<<temp -> data<<" | <=>";
+        temp = temp -> next;
+    }
+    cout<<"NULL \n";
+}
+
+template <class T>
+int DoublyLLL<T> :: Count()
+{
+    return this -> iCount;
+}
+
+///////////////////////////////////////////////////////////
+//
+//       Stack using Generic Approach
+//
+///////////////////////////////////////////////////////////
+
+#pragma pack(1)
+template <class T>
+class Stacknode
+{
+    public:
+        T data;
+        Stacknode<T> *next;
+
+        Stacknode(T no)
+        {
+            this -> data = no;
+            this -> next = NULL;
+        }
+};
+
+template <class T>
+class Stack
+{
+    private:
+        Stacknode<T> *first;
+        int iCount;
+
+    public:
+        Stack();
+
+        void push(T);         
+        T pop();              
+        T peep();
+        void Display();
+        int Count();
+};
+
+template <class T>
+Stack<T> :: Stack()
+{
+    cout<<"Stack gets Created Successfully... \n";
+    this -> first = NULL;
+    this -> iCount = 0;
+}
+
+template <class T>
+void Stack<T> :: push(T no)
+{
+    Stacknode<T> *newn = NULL;
+
+    newn = new Stacknode<T>(no);
+
+    newn -> next = this -> first;
+    this -> first = newn;
+
+    this -> iCount++;
+
+}  
+
+template <class T>
+T Stack<T> :: pop() 
+{
+    T Value = 0;
+    Stacknode<T> *temp = this -> first;
+
+    if(this -> first == NULL)
+    {
+        cout<<"Stack is Empty \n";
+        return -1;
+    }
+
+    Value = this -> first -> data;
+
+    this -> first = this -> first -> next;
+    delete temp;
+
+    this -> iCount--;
+
+    return Value;
+
+}  
+
+template <class T>
+T Stack<T> :: peep()
+{
+    T Value = 0;
+
+    if(this->first == NULL)
+    {
+        cout<<"Stack is Empty \n";
+        return -1;
+    }
+
+    Value = this -> first -> data;
+    
+    return Value;
+}
+
+template <class T>
+void Stack<T> :: Display()
+{
+    Stacknode<T> *temp = this -> first;
+
+
+    if(this -> first == NULL)
+    {
+        cout<<"Stack is Empty \n";
+        return;
+    }
+
+    while(temp != NULL)
+    {
+        cout<<"| \t"<<temp -> data<<"\t | \n";
+        temp = temp -> next;
+    }
+}
+
+template <class T>
+int Stack<T> :: Count()
+{
+    return this -> iCount;
+}
+
+///////////////////////////////////////////////////////////
+//
+//       Queue using Generic Approach
+//
+///////////////////////////////////////////////////////////
+
+#pragma pack(1)
+template <class T>
+class Queuenode
+{
+    public:
+        T data;
+        Queuenode<T> *next;
+
+        Queuenode(T no)
+        {
+            this -> data = no;
+            this -> next = NULL;
+        }
+};
+
+template <class T>
+class Queue
+{
+    private:
+        Queuenode<T> *first;
+        Queuenode<T> *last;
+
+        int iCount;
+
+    public:
+        Queue();
+
+        void enqueue(T);             
+        T dequeue();          
+        void Display();
+        int Count();
+};
+
+template<class T>
+Queue<T> :: Queue()
+{
+    cout<<"Queue gets Created Successfully... \n";
+
+    this -> first = NULL;
+    this -> last = NULL;
+    this -> iCount = 0;
+}
+
+template <class T>
+void Queue<T> :: enqueue(T no)
+{
+    Queuenode<T> *newn = NULL;
+
+    newn = new Queuenode<T>(no);
+
+    if((this -> first == NULL) && (this -> last == NULL))
+    {
+        this -> first = newn;
+        this -> last = newn;
+    }
+
+    else
+    {
+        this -> last -> next = newn;
+        this -> last = this -> last -> next;          // this->last = newn;
+    }
+    
+    this -> iCount++;
+} 
+
+template <class T>
+T Queue<T> :: dequeue() 
+{
+    T Value = 0;
+    Queuenode<T> *temp = this -> first;
+
+    if((this -> first == NULL) && (this -> last == NULL))
+    {
+        cout<<"Queue is Empty \n";
+        return -1;
+    }
+
+    Value = this -> first -> data;
+
+    this -> first = this -> first -> next;
+    delete temp;
+
+    this -> iCount--;
+
+    return Value;
+}           
+
+template <class T>
+void Queue<T> :: Display()
+{
+    Queuenode<T> *temp = this -> first;
+
+    if((this -> first == NULL) && (this -> last == NULL))
+    {
+        cout<<"Queue is Empty \n";
+        return;
+    }
+
+    while(temp != NULL)
+    {
+        cout<<"| "<<temp -> data<<" | - ";
+        temp = temp -> next;
+    }
+    cout<<"\n";
+}
+
+template <class T>
+int Queue<T> :: Count()
+{
+    return this -> iCount;
+}
+
+/////////////////////////////////////   END of Library   //////////////////////////////////////////////////
+
+int main()
+
+{
+    SinglyLLL<int> *obj = new SinglyLLL<int>();
+    int iRet = 0;
+
+    obj -> InsertFirst(51);
+    obj -> InsertFirst(21);
+    obj -> InsertFirst(11);
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+
+    obj -> InsertLast(101);
+    obj -> InsertLast(111);
+    obj -> InsertLast(121);
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+
+    obj -> DeleteFirst();
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+
+    obj -> DeleteLast();
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+
+    obj -> InsertAtPos(105,4);
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+
+    obj -> DeleteAtPos(4);
+    obj -> Display();
+    iRet = obj -> Count();
+    cout<<"Number of Nodes are : " <<iRet<< "\n";
+    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    DoublyLLL<char> *dobj = new DoublyLLL<char>();
+
+    dobj -> InsertFirst('A');
+    dobj -> InsertFirst('B');
+    dobj -> InsertFirst('C');
+    dobj -> Display();
+    cout<<"Number of Elements Are : " <<dobj -> Count()<< "\n";
+
+    dobj -> InsertLast('X');
+    dobj -> InsertLast('Y');
+    dobj -> InsertLast('Z');
+    dobj -> Display();
+    cout<<"Number of Elements Are : "<<dobj -> Count()<<"\n";
+
+    dobj -> DeleteFirst();
+    dobj -> Display();
+    cout<<"Number of Elements Are : "<< dobj -> Count()<< "\n";
+
+    dobj -> DeleteLast();
+    dobj -> Display();
+    cout<<"Number of Elements Are : "<< dobj -> Count()<< "\n";
+
+    dobj -> InsertAtPos('$',4);
+    dobj -> Display();
+    cout<<"Number of Elements Are : "<< dobj -> Count()<< "\n";
+
+    dobj -> DeleteAtPos(4);
+    dobj -> Display();
+    cout<<"Number of Elements Are : "<< dobj->Count()<< "\n";
+
+    delete dobj;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   
+    Stack<char> *sobj = new Stack<char>();
+
+    sobj -> push('a');
+    sobj -> push('b');
+    sobj -> push('c');
+    sobj -> push('d');
+    sobj -> Display();
+    cout<<"Number of Elements is Stack are : "<< sobj -> Count()<< "\n";
+
+    cout<<"Return value of Peep is : "<< sobj -> peep()<< "\n";
+    sobj -> Display();
+    cout<<"Number of Elements is Stack are : "<< sobj -> Count()<< "\n";
+
+    cout<<"Poped Element is : "<<sobj -> pop() << "\n";
+    sobj -> Display();
+    cout<<"Number of Elements is Stack are : "<< sobj -> Count()<< "\n";
+
+    cout<<"Poped Element is : "<<sobj -> pop()<<"\n";
+    sobj -> Display();
+    cout<<"Number of Elements is Stack are :"<< sobj -> Count()<< "\n";
+    
+    sobj -> push('e');
+    sobj -> Display();
+    cout<<"Number of Elements is Stack are :"<< sobj -> Count()<< "\n";
+
+    delete sobj;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Queue<double> *qobj = new Queue<double>();
+
+    qobj -> enqueue(11.5689);
+    qobj -> enqueue(21.56789);
+    qobj -> enqueue(51.5689);
+    qobj -> enqueue(101.56789);
+    qobj -> Display();
+    cout<<"Number of Elements is Queue are : "<<qobj -> Count()<<"\n";
+
+    cout<<"Removed Element is : "<<qobj -> dequeue()<<"\n";
+    qobj -> Display();
+    cout<<"Number of Elements is Queue are : "<<qobj -> Count()<<"\n";
+
+    cout<<"Removed Element is : "<<qobj -> dequeue()<<"\n";
+    qobj -> Display();
+    cout<<"Number of Elements is Queue are : "<<qobj -> Count()<<"\n";
+    
+    qobj -> enqueue(121.5689);
+    qobj -> Display();
+    cout<<"Number of Elements is Queue are : "<<qobj -> Count()<<"\n";
+
+    delete qobj;
+
+    return 0;
+}
